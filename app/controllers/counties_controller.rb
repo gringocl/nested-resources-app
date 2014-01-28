e53bffed235419f21d05ca9ad22c51a466dcad3a
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-01-28 00:22:21 -0800

    Code Review Thougths

--- app/controllers/counties_controller.rb
+++ app/controllers/counties_controller.rb
@@ -1,81 +1,52 @@
 class CountiesController < ApplicationController
-  #before_action :set_county, only: [:show, :edit, :update, :destroy]
-  before_action :set_state, only: [:index, :show, :new, :edit, :create, :update]
+  before_action :set_state
+  before_action :set_county, only: [:show, :edit, :destroy, :update]
 
-
-  # GET /state/:state_id/counties
   def index
     @counties = @state.counties
   end
 
-  # GET /states/:state_id/counties/:id
   def show
-    @county = @state.counties.find(params[:id])
   end
 
-  # GET /states/:state_id/counties/new
   def new
     @county = @state.counties.new
   end
 
-  # GET /states/:state_id/counties/:id/edit
   def edit
-    @county = @state.counties.find(params[:id])
   end
 
-  # POST /states/:state_id/counties/:id
   def create
-    @county = @state.counties.new(county_params)
-
-    respond_to do |format|
-      if @county.save
-        format.html { redirect_to state_county_path(@state, @county), notice: 'County was successfully created.'}
-        format.json { render action: 'show', status: :created, location: @county }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @county.errors, status: :unprocessable_entity }
-      end
+    @county = @state.counties.create(county_params)
+    if @county.save
+      redirect_to [ @state, @county ], notice: 'County was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
-  # PATCH/PUT /state/:state_id/counties/:id
   def update
-    @county = County.find(params[:id])
-
-    respond_to do |format|
-      if @county.update(county_params)
-        format.html { redirect_to ([@state, @county]), notice: 'County was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @county.errors, status: :unprocessable_entity }
-      end
+    if @county.update(county_params)
+      redirect_to [@state, @county], notice: 'County was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
-  # DELETE /counties/1
-  # DELETE /counties/1.json
   def destroy
-    @county = County.find(params[:id])
     @county.destroy
-    respond_to do |format|
-      format.html { redirect_to state_counties_url }
-      format.json { head :no_content }
-    end
+    redirect_to state_counties_url
   end
 
   private
-
-    # Use callbacks to share common setup or constraints between actions.
     def set_state
       @state = State.find(params[:state_id])
     end
 
     def set_county
-      @county = County.find(params[:id])
+      @county = @state.counties.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def county_params
       params.require(:county).permit(:name, :population, :state_id)
     end
